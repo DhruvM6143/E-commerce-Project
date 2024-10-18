803c6047c06a649dc92c4ea22ebbf568d5ee3eaa
Dhruv Madaan <madaandhruv50@gmail.com>
2024-10-18 09:56:48 +0530

    fixed error in admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -32,8 +32,8 @@ const App = () => {
               <SideBar />
               <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
                 <Routes>
+                  <Route path='/' element={<List token={token} />} />
                   <Route path='/add' element={<Add token={token} />} />
-                  <Route path='/list' element={<List token={token} />} />
                   <Route path='/orders' element={<Orders token={token} />} />
                 </Routes>
               </div>
