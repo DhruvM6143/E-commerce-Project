876d2751f8f8d7a8fa8dc65a57fa9a472d80747b
Dhruv Madaan <madaandhruv50@gmail.com>
2024-11-04 22:23:35 +0530

    typo

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -37,7 +37,7 @@ const Login = ({ loading }) => {
         setPassword(passwordValue)
 
         const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        setIsValid(passwordPattern.test(passwordValue))
+        setIsPasswordValid(passwordPattern.test(passwordValue))
     }
 
 
