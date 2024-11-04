968243c13df0cc0bb97047e6cb01b73ff653317d
Dhruv Madaan <madaandhruv50@gmail.com>
2024-11-04 22:40:41 +0530

    fixed regex error

--- frontend/src/ResetPassword.jsx
+++ frontend/src/ResetPassword.jsx
@@ -7,25 +7,48 @@ import { ShopContext } from './Context/ShopContext'
 import Loading from './Components/Loading'
 const ResetPassword = ({ loading }) => {
     const [password, setPassword] = useState('')
+    const [isValidP, setIsValidP] = useState(true)
+    const [isValidCp, setIsValidCp] = useState(true)
     const { backendUrl, navigate } = useContext(ShopContext)
     const [confirmPassword, setConfirmPassword] = useState('')
     const { token } = useParams()
+
+    const passwordChange = (e) => {
+        const passwordValue = e.target.value;
+        setPassword(passwordValue)
+
+        const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        setIsValidP(passwordPattern.test(passwordValue))
+    }
+    const confirmPasswordChange = (e) => {
+        const passwordValue = e.target.value;
+        setConfirmPassword(passwordValue)
+
+        const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        setIsValidCp(passwordPattern.test(passwordValue))
+    }
     const submitHandler = async (e) => {
         e.preventDefault()
-        try {
-            if (password !== confirmPassword) {
-                toast.error("Password does not match")
-                return;
-            }
-            const response = await axios.post(backendUrl + `/api/user/reset-password/${token}`, { password })
-            if (response.data.success) {
-                toast.success("Password changed successfully")
-                navigate("/login")
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error(error.message)
+        if (isValidCp && isValidP) {
+            try {
+                if (password !== confirmPassword) {
+                    toast.error("Password does not match")
+                    return;
+                }
+                const response = await axios.post(backendUrl + `/api/user/reset-password/${token}`, { password })
+                if (response.data.success) {
+                    toast.success("Password changed successfully")
+                    navigate("/login")
+                }
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)
 
+            }
+        }
+        else {
+            toast.error("Invalid Password. Password must contain at least 8 characters, including uppercase and lowercase letters, numbers, and special characters.")
+            return;
         }
 
     }
@@ -40,11 +63,11 @@ const ResetPassword = ({ loading }) => {
                         <form onSubmit={submitHandler}>
                             <div>
                                 <p>Enter your new Password</p>
-                                <input onChange={(e) => setPassword(e.target.value)} value={password} className='w-full px-3 py-2 border border-gray-800' type="text" />
+                                <input onChange={passwordChange} value={password} className='w-full px-3 py-2 border border-gray-800' type="text" />
                             </div>
                             <div>
                                 <p>Re-Enter your Password</p>
-                                <input onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} className='w-full px-3 py-2 border border-gray-800' type="text" />
+                                <input onChange={confirmPasswordChange} value={confirmPassword} className='w-full px-3 py-2 border border-gray-800' type="text" />
                             </div>
                             <button type='submit' className='bg-black text-white font-light px-8 py-2 mt-4'>Change Password</button>
                         </form>
